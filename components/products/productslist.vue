d07b4a4d429f05fd1b06ba582ad0433b66eed102
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-03-11 08:51:17 +0100

    Add skeleton loader for products

--- components/products/productslist.vue
+++ components/products/productslist.vue
@@ -5,15 +5,17 @@ type Product = {
 	description?: string;
 	category: { name: string };
 };
+
 let products = ref(<Product[]>[]);
 let activeCard = ref(<string | null>null);
+let loading = ref(true);
 
 const fetchProducts = async () => {
 	try {
 		const response = await $fetch("/api/products", { method: "GET" });
 		if (response.status === "success") {
 			products.value = response.products;
-			console.log(products.value);
+			loading.value = false;
 		} else products.value = <Product[]>[];
 	} catch (error) {
 		console.error(error);
@@ -28,13 +30,16 @@ fetchProducts();
 		<v-container fluid>
 			<v-row dense>
 				<v-col cols="12" sm="6" md="4" lg="3" v-for="(product, index) in products" :key="index">
-					<v-card :style="{ minHeight: '200px' }" :color="'blue-darken-4'" :variant="'flat'" class="product" hover @mouseenter="activeCard = product.name" @mouseleave="activeCard = null">
+					<v-card :style="{ minHeight: '200px' }" color="blue-darken-4" :variant="'elevated'" class="product text-white" hover @mouseenter="activeCard = product.name" @mouseleave="activeCard = null">
 						<v-card-item>
 							<v-card-title>{{ product.name }} </v-card-title>
 							<v-card-subtitle>{{ product.category.name }} </v-card-subtitle>
 							<v-card-text v-if="product.description"> {{ product.description }} </v-card-text>
 						</v-card-item>
 					</v-card>
+				</v-col>
+				<v-col v-if="loading" cols="12" sm="6" md="4" lg="3" v-for="(_, index) in 5" :key="index">
+					<v-skeleton-loader type="card"></v-skeleton-loader>
 				</v-col>
 			</v-row>
 		</v-container>
