859e6b6f2dd3edc89c76d32bfadf4110e8fe503d
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-03-11 03:26:29 +0100

    Add trycatch block in products

--- components/products/productslist.vue
+++ components/products/productslist.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref } from "vue";
 type Product = {
 	name: string;
 	description?: string;
@@ -9,11 +9,15 @@ let products = ref(<Product[]>[]);
 let activeCard = ref(<string | null>null);
 
 const fetchProducts = async () => {
-	const response = await $fetch("/api/products", { method: "GET" });
-	if (response.status === "success") {
-		products.value = response.products;
-		console.log(products.value);
-	} else products.value = <Product[]>[];
+	try {
+		const response = await $fetch("/api/products", { method: "GET" });
+		if (response.status === "success") {
+			products.value = response.products;
+			console.log(products.value);
+		} else products.value = <Product[]>[];
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 fetchProducts();
@@ -24,14 +28,7 @@ fetchProducts();
 		<v-container fluid>
 			<v-row dense>
 				<v-col cols="12" sm="6" md="4" lg="3" v-for="(product, index) in products" :key="index">
-					<v-card
-						:style="{ minHeight: '200px' }"
-						:color="'blue-darken-4'"
-						:variant="'flat'"
-						class="product"
-						hover
-						@mouseenter="activeCard = product.name"
-						@mouseleave="activeCard = null">
+					<v-card :style="{ minHeight: '200px' }" :color="'blue-darken-4'" :variant="'flat'" class="product" hover @mouseenter="activeCard = product.name" @mouseleave="activeCard = null">
 						<v-card-item>
 							<v-card-title>{{ product.name }} </v-card-title>
 							<v-card-subtitle>{{ product.category.name }} </v-card-subtitle>
