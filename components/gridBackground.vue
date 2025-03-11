73b2f1d56c1e2d990bf7c9504fc404fa00ac263b
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2025-03-11 09:17:19 +0100

    decreased opacity of the grid for improved visual clarity

--- components/gridBackground.vue
+++ components/gridBackground.vue
@@ -12,13 +12,14 @@ const props = defineProps<{
 	<div
 		:style="{
 			width: props.size,
-	height: props.size,
+			height: props.size,
 			maxWidth: '100vw',
 			margin: 'auto',
-      position: 'absolute',
+			position: 'absolute',
 			background: `linear-gradient(to right, gray 1px, transparent 1px ), linear-gradient(to top, gray 1px, transparent 1px)`,
-      maskImage: 'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 60%)',
+			maskImage: 'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 60%)',
 			backgroundSize: `${props.gridSize} ${props.gridSize}`,
 			backgroundRepeat: 'repeat',
+			opacity: 0.4
 		}"></div>
 </template>
